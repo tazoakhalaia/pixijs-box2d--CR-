14ff05e7e17f07e689a1d660c287ca424cf32ba6
tazoakhalaia <darknemiroff@gmail.com>
2024-10-22 12:14:47 +0400

    change physics engine

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,12 @@
-import Matter, { Composite, Mouse, MouseConstraint, Runner } from "matter-js";
+import {
+  b2BodyDef,
+  b2BodyType,
+  b2FixtureDef,
+  b2MouseJointDef,
+  b2PolygonShape,
+  b2Vec2,
+  b2World,
+} from "box2d.ts";
 import {
   Application,
   Assets,
@@ -11,10 +19,10 @@ import {
 class Canvas {
   private app = new Application();
   private canvas = document.querySelector(".app");
-  private engine = Matter.Engine.create();
-  private mouseConstraint?: MouseConstraint;
   private cont = new Container();
-
+  private world = new b2World(new b2Vec2(0, 10));
+  private selectedBox: any = null;
+  private mouseJoint: any = null;
   private assets = {
     insta:
       "https://upload.wikimedia.org/wikipedia/commons/9/95/Instagram_logo_2022.svg",
@@ -27,25 +35,6 @@ class Canvas {
     },
   });
 
-  private ground = Matter.Bodies.rectangle(300, 580, 600, 50, {
-    isStatic: true,
-  });
-  private leftWall = Matter.Bodies.rectangle(0, 300, 40, 600, {
-    isStatic: true,
-  });
-  private rightWall = Matter.Bodies.rectangle(600, 300, 40, 600, {
-    isStatic: true,
-  });
-  private ceiling = Matter.Bodies.rectangle(300, 0, 600, 40, {
-    isStatic: true,
-  });
-  private box = Matter.Bodies.rectangle(45, 100, 150, 50);
-  private box1 = Matter.Bodies.rectangle(230, 150, 150, 50);
-  private box2 = Matter.Bodies.rectangle(250, 0, 150, 50);
-  private box3 = Matter.Bodies.rectangle(420, 0, 150, 50);
-  private box4 = Matter.Bodies.rectangle(340, 20, 160, 50);
-  private box5 = Matter.Bodies.rectangle(500, 300, 160, 50);
-
   constructor() {
     Assets.addBundle("assets", this.assets);
   }
@@ -55,7 +44,7 @@ class Canvas {
       await this.app.init({
         width: 610,
         height: 600,
-        backgroundColor: 'green',
+        backgroundColor: "green",
         backgroundAlpha: 1,
         antialias: true,
       });
@@ -63,10 +52,6 @@ class Canvas {
         this.canvas?.appendChild(this.app.canvas);
       }
       (globalThis as any).__PIXI_APP__ = this.app;
-
-      const boxGraphics = new Graphics()
-        .roundRect(-90, -25, 180, 50, 30)
-        .fill({ color: "#ea19c9" });
 
       const insta = new Sprite(Assets.get("insta"));
       insta.anchor.set(0.5, 0.5);
@@ -77,133 +62,128 @@ class Canvas {
 
       this.isntaText.anchor.set(0.5, 0.5);
 
-      this.cont.addChild(boxGraphics, insta, this.isntaText);
+      this.createStaticBox(300, 580, 600, 50);
+      this.createStaticBox(0, 300, 40, 600);
+      this.createStaticBox(600, 300, 40, 600);
+      this.createStaticBox(300, 0, 600, 40);
 
-      const boxGraphics1 = new Graphics()
-        .roundRect(-75, -25, 150, 50, 30)
-        .fill({ color: "#ea19c9" });
-      const boxGraphics2 = new Graphics()
-        .roundRect(-75, -25, 150, 50, 30)
-        .fill({ color: "#ea19c9" });
-
-      const boxGraphics3 = new Graphics()
-        .roundRect(-75, -25, 150, 50, 30)
-        .fill({ color: "#ea19c9" });
-      const boxGraphics4 = new Graphics()
-        .roundRect(-80, -25, 160, 50, 30)
-        .fill({ color: "#ea19c9" });
-      const boxGraphics5 = new Graphics()
-        .roundRect(-80, -25, 160, 50, 30)
-        .fill({ color: "#ea19c9" });
-
-      const groundGraphics = new Graphics()
-        .rect(-300, -20, 600, 40)
-        .fill({ color: "transparent" });
-
-      const leftWallGraphics = new Graphics()
-        .rect(-20, -300, 40, 600)
-        .fill({ color: "transparent" });
-
-      const rightWallGraphics = new Graphics()
-        .rect(-20, -300, 40, 600)
-        .fill({ color: "transparent" });
-
-      const ceilingGraphics = new Graphics()
-        .rect(-300, -20, 600, 40)
-        .fill({ color: "transparent" });
-
-      this.app.stage.addChild(
-        groundGraphics,
-        this.cont,
-        boxGraphics1,
-        boxGraphics2,
-        boxGraphics3,
-        boxGraphics4,
-        boxGraphics5,
-        leftWallGraphics,
-        rightWallGraphics,
-        ceilingGraphics
-      );
+      const dynamicBoxes = [
+        this.createDynamicBox(45, 100, 150, 50),
+        this.createDynamicBox(230, 150, 150, 50),
+        this.createDynamicBox(250, 0, 150, 50),
+        this.createDynamicBox(420, 0, 150, 50),
+        this.createDynamicBox(340, 20, 160, 50),
+        this.createDynamicBox(500, 300, 160, 50),
+      ];
+      const boxGraphics = dynamicBoxes.map(() => {
+        const box = new Graphics()
+          .roundRect(-80, -25, 160, 50, 30)
+          .fill({ color: 0xea19c9 });
+        return box;
+      });
+      this.cont.addChild(insta);
+      this.app.stage.addChild(this.cont, ...boxGraphics);
 
       this.app.ticker.add(() => {
-        this.cont.x = this.box.position.x;
-        this.cont.y = this.box.position.y;
-        this.cont.rotation = this.box.angle;
-
-        boxGraphics1.x = this.box1.position.x;
-        boxGraphics1.y = this.box1.position.y;
-        boxGraphics1.rotation = this.box1.angle;
-
-        boxGraphics2.x = this.box2.position.x;
-        boxGraphics2.y = this.box2.position.y;
-        boxGraphics2.rotation = this.box2.angle;
-
-        boxGraphics3.x = this.box3.position.x;
-        boxGraphics3.y = this.box3.position.y;
-        boxGraphics3.rotation = this.box3.angle;
-
-        boxGraphics4.x = this.box4.position.x;
-        boxGraphics4.y = this.box4.position.y;
-        boxGraphics4.rotation = this.box4.angle;
-
-        boxGraphics5.x = this.box5.position.x;
-        boxGraphics5.y = this.box5.position.y;
-        boxGraphics5.rotation = this.box5.angle;
-
-        groundGraphics.x = this.ground.position.x;
-        groundGraphics.y = this.ground.position.y;
-        groundGraphics.rotation = this.ground.angle;
-
-        leftWallGraphics.x = this.leftWall.position.x;
-        leftWallGraphics.y = this.leftWall.position.y;
-
-        rightWallGraphics.x = this.rightWall.position.x;
-        rightWallGraphics.y = this.rightWall.position.y;
-
-        ceilingGraphics.x = this.ceiling.position.x;
-        ceilingGraphics.y = this.ceiling.position.y;
+        this.world.Step(1 / 30, 10, 10);
+        dynamicBoxes.forEach((body, index) => {
+          const position = body.GetPosition();
+          const angle = body.GetAngle();
+          const graphic = boxGraphics[index];
+          graphic.x = position.x * 30;
+          graphic.y = position.y * 30;
+          graphic.rotation = angle;
+        });
       });
-
-      const mouse = Mouse.create(this.app.canvas);
-      this.mouseConstraint = MouseConstraint.create(this.engine, {
-        mouse: mouse,
-        constraint: {
-          stiffness: 0.2,
-        },
-      });
-
-      Matter.Events.on(this.mouseConstraint, "mouseup", () => {
-        if (this.mouseConstraint)
-          Matter.World.remove(this.engine.world, this.mouseConstraint);
-      });
-
-      Matter.Events.on(this.mouseConstraint, "mousedown", () => {
-        if (this.mouseConstraint)
-          Matter.World.add(this.engine.world, this.mouseConstraint);
-      });
-
-      Composite.add(this.engine.world, [
-        this.ground,
-        this.leftWall,
-        this.rightWall,
-        this.ceiling,
-        this.box,
-        this.box1,
-        this.box2,
-        this.box3,
-        this.box4,
-        this.box5,
-        this.mouseConstraint,
-      ]);
-
-      const runner = Runner.create();
-      Matter.Runner.run(runner, this.engine);
+      this.setupMouseEvents(dynamicBoxes, boxGraphics);
     });
   }
 
-  addMouseConstraint() {
-    if (this.mouseConstraint)
-      Matter.World.add(this.engine.world, this.mouseConstraint);
+  setupMouseEvents(dynamicBoxes: any[], boxGraphics: any[]) {
+    let mouseX = 0,
+      mouseY = 0;
+
+    this.app.view.addEventListener("mousedown", (event: MouseEvent) => {
+      mouseX = event.offsetX / 30;
+      mouseY = event.offsetY / 30;
+
+      dynamicBoxes.forEach((box) => {
+        const fixture = box.GetFixtureList();
+        const shape = fixture.GetShape();
+
+        const isInside = shape.TestPoint(
+          box.GetTransform(),
+          new b2Vec2(mouseX, mouseY)
+        );
+        if (isInside) {
+          this.selectedBox = box;
+          this.createMouseJoint(mouseX, mouseY, box);
+        }
+      });
+    });
+
+    this.app.canvas.addEventListener("mousemove", (event: MouseEvent) => {
+      if (this.mouseJoint) {
+        mouseX = event.offsetX / 30;
+        mouseY = event.offsetY / 30;
+        this.mouseJoint.SetTarget(new b2Vec2(mouseX, mouseY));
+      }
+    });
+
+    this.app.canvas.addEventListener("mouseup", () => {
+      if (this.mouseJoint) {
+        this.world.DestroyJoint(this.mouseJoint);
+        this.mouseJoint = null;
+        this.selectedBox = null;
+      }
+    });
+
+    this.app.canvas.addEventListener("mouseleave", () => {
+      if (this.mouseJoint) {
+        this.world.DestroyJoint(this.mouseJoint);
+        this.mouseJoint = null;
+        this.selectedBox = null;
+      }
+    });
+  }
+
+  createMouseJoint(mouseX: number, mouseY: number, body: any) {
+    const mouseJointDef = new b2MouseJointDef();
+    mouseJointDef.bodyA = this.world.CreateBody(new b2BodyDef());
+    mouseJointDef.bodyB = body;
+    mouseJointDef.target.Set(mouseX, mouseY);
+    mouseJointDef.maxForce = 1000.0 * body.GetMass();
+    this.mouseJoint = this.world.CreateJoint(mouseJointDef);
+  }
+  ////
+
+  createStaticBox(x: number, y: number, width: number, height: number) {
+    const bodyDef = new b2BodyDef();
+    bodyDef.position.Set(x / 30, y / 30);
+    bodyDef.type = b2BodyType.b2_staticBody;
+
+    const shape = new b2PolygonShape();
+    shape.SetAsBox(width / 30 / 2, height / 30 / 2);
+
+    const body = this.world.CreateBody(bodyDef);
+    body.CreateFixture(shape, 0);
+  }
+  createDynamicBox(x: number, y: number, width: number, height: number) {
+    const bodyDef = new b2BodyDef();
+    bodyDef.position.Set(x / 30, y / 30);
+    bodyDef.type = b2BodyType.b2_dynamicBody;
+
+    const shape = new b2PolygonShape();
+    shape.SetAsBox(width / 30 / 2, height / 30 / 2);
+
+    const body = this.world.CreateBody(bodyDef);
+    const fixtureDef = new b2FixtureDef();
+    fixtureDef.shape = shape;
+    fixtureDef.density = 1.0;
+    fixtureDef.friction = 0.3;
+    body.CreateFixture(fixtureDef);
+
+    return body;
   }
 }
 
